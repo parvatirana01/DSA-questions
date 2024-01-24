72b173b4d5c5ba82490f0d1d92d7cbb756f16267
Parvati Rana <parvatirana6780@gmail.com>
2024-01-24 12:56:56 +0530

    Time: 13 ms (67.52%), Space: 23.3 MB (5.50%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int max=0;
+        unordered_map<int,int> freq;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(freq[nums[i]]>n/2){
+               max=nums[i];
+            }
+        }
+    return max;
+    }
+};
